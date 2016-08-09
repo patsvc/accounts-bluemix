d6c2dcfbe901acefe8301585e48f154f0c0b6d83
patsvc <patsvc@us.ibm.com>
2016-08-09 14:09:10 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'ibmcloud:accounts-bluemix',
+  name: 'patsvc:accounts-bluemix',
   summary: "Login service for Bluemix accounts",
   version: "1.0.4",
   git: 'https://github.com/oneibmcloud/accounts-bluemix.git'
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('ibmcloud:bluemix@1.0.4', ['client', 'server']);
+  api.use('patsvc:bluemix@1.0.4', ['client', 'server']);
 
   api.addFiles('bluemix_login_button.css', 'client');
   api.addFiles("bluemix.js");
