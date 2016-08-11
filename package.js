3174251f8c0cf746405d495a7d21fef9acadd31a
saurabh agarwal <agarwasa@us.ibm.com>
2016-08-11 16:22:07 -0400

    changed the dependency on blue mix 1.0.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'patsvc:accounts-bluemix',
   summary: "Login service for Bluemix accounts",
-  version: "1.0.4",
+  version: "1.0.5",
   git: 'https://github.com/patsvc/accounts-bluemix.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('patsvc:bluemix@1.0.4', ['client', 'server']);
+  api.use('patsvc:bluemix@1.0.5', ['client', 'server']);
 
   api.addFiles('bluemix_login_button.css', 'client');
   api.addFiles("bluemix.js");
